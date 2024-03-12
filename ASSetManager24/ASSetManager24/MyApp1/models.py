855fc2aa5c9c40874e6f20dad94418c5376be3d1
dr-mousetrap <141705706+dr-mousetrap@users.noreply.github.com>
2024-03-12 14:17:46 +1100

    Update models.py

--- ASSetManager24/ASSetManager24/MyApp1/models.py
+++ ASSetManager24/ASSetManager24/MyApp1/models.py
@@ -3,4 +3,4 @@ from django.db import models
 # Create your models here.
 class teacher(models.Model):
     Name = models.CharField(max_length=25)
-    Area = models.CharField(max_length=30
+    Area = models.CharField(max_length=30)
